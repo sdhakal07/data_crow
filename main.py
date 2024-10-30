aa3d8765ec9b350d477550f41f91b87ee4ae6350
SDHAKAL07 <dhakalsuman07@gmail.com>
2024-10-30 09:38:30 +0545

    D_UTL 03: Excelutility object initiation logic addition to main.py file

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-from ExcelUtility import  excelutility as excel_util
-excel_util = excel_util.convert_to_csv("data.xlsx")
+from ExcelUtility import excelutility as excel_util
+file =excel_util.ExcelUtility(r"C:\Users\example.xlsx")
+excel_util = file.convert_to_csv("Output\data.csv")
